b992bb2b7951121d9f60cf0300f4003d2c179815
Jon Magnus Eier <joeier@proton.me>
2026-09-17 19:42:45 +0200

    IN1900: Python notes

---
+++ notater/filer.py
@@ -0,0 +1,9 @@
+# with open("#filbane", "#argumenter"):
+
+
+
+# - Argumenter inkluderer
+#     - r for lesing av fil. Error om fil ikke finnes. Standard argument
+#     - w for skriving til fil. Lager fil om ikke finnes
+#     - a for å legge til noe til en fil. Lager fil om ikke finnes
+#     - x for å lage fil. Error om fil allerede finnes

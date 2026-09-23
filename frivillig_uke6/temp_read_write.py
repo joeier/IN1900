29c6f11431cae778931d7ef74c420585164167fb
Jon Magnus Eier <joeier@proton.me>
2026-09-23 12:55:48 +0200

    completed 1st assignment

--- frivillig_uke6/temp_read_write.py
+++ frivillig_uke6/temp_read_write.py
@@ -3,7 +3,10 @@ import numpy as np
 
 def extract_data(filename):
 
-    temp_list = []
+    temp_data = {
+        "filename": filename,
+        "temps": []
+        }
 
     with open(filename) as f:
 
@@ -13,37 +16,40 @@ def extract_data(filename):
 
             for temp in line.split():
 
-                temp_list.append(float(temp))
+                temp_data["temps"].append(float(temp))
 
-    return temp_list
+    return temp_data
 
 
 def make_weather_data(data):
 
-    avg = float(np.mean(data))
+    avg = float(np.mean(data["temps"]))
 
-    dataset = {
-        "Gjennomsnittlig": round(avg, 1),
-        "Minste": np.min(data),
-        "Største": np.max(data)
-        }
+    data["Average"] = round(avg, 1)
+    data["Lowest"] = np.min(data["temps"])
+    data["Highest"] = np.max(data["temps"])
 
-    return dataset
+    return data
 
 
 def print_data(dataset):
-    print("Temperature data from ")
+
+    time = dataset["filename"].split("_")
+
+    month = time[1]
+    year = time[2].split(".")[0]
+
+    print(f"Temperature for {month} {year}")
+
+    for key, value in list(dataset.items())[2:]:
+        
+        print(f"{key}: {value} degrees celsius")
+
+    print("\n")
 
 
 oct_1945 = make_weather_data(extract_data("temp_oct_1945.txt"))
 oct_2014 = make_weather_data(extract_data("temp_oct_2014.txt"))
 
-print("Temperaturdata fra oktober 1945")
-for key, value in oct_1945.items():
-    print(f"{key}: {value} grader celsius")
-
-print("\n")
-
-print("Temperaturdata fra oktober 2014")
-for key, value in oct_2014.items():
-    print(f"{key}: {value} grader celsius")
+print_data(oct_1945)
+print_data(oct_2014)
